76c33b829f98b0add0139214072edc84fb591c20
ItsMeMisha <m.shlyahin@rambler.ru>
2019-10-24 18:08:01 +0300

    Add some functional for labels

--- AsmAndCPU/disasm.cpp
+++ AsmAndCPU/disasm.cpp
@@ -5,18 +5,31 @@
 #include <malloc.h>
 #include <string.h>
 
+const char FileInDefault[] = "MyProg.myexe";
+const char FileOutDefault[] = "ProgText.out";
+
+struct label {
+
+  char name[256];
+    int place;
+
+}; 
+
 void PrintArgs (FILE* file, char** code, int numOfArgs);
 
 int main (int argc, char* argv[]) {
 
-   	char* FileOutName = "ProgText.out";
-	char* FileInName = "MyProg.myexe";
+   	char FileOutName[256] = "";
+    strcpy (FileOutName, FileOutDefault);
+
+	char FileInName[256] = "";
+    strcpy (FileInName, FileInDefault);
 
 	if (argc > 1)
-		FileInName = argv[1];
+		strcpy (FileInName, argv[1]);
 
 	if (argc > 2)
-		FileOutName = argv[2];
+		strcpy (FileOutName, argv[2]);
 	
 	FILE* FileIn = fopen (FileInName, "rb");
 
@@ -35,6 +48,10 @@ int main (int argc, char* argv[]) {
     if (FileInfoPtr -> st_size > 3)
         content += 3;
 
+    label LabelsArr[256] = {};
+
+    int LabelsNum = 0;
+
     const char* FileBegin = content - 3;
 
     fclose (FileIn);
@@ -43,13 +60,11 @@ int main (int argc, char* argv[]) {
 
     assert (FileOut);
 
-    int lineCounter = 0;
-
     #define DEF_CMD(name, cmdNum, numOfArgs, codeForCpu)    \
                                                             \
         case CMD_##name: {                                  \
                                                             \
-            fprintf (FileOut, "%d %s ", lineCounter, #name);\
+            fprintf (FileOut, "%s ", #name);                \
                                                             \
             if (numOfArgs > 0)                              \
                 PrintArgs (FileOut, &content, numOfArgs);   \
@@ -58,19 +73,25 @@ int main (int argc, char* argv[]) {
                 ++content;                                  \
            }                                                \
                                                             \
-            ++lineCounter;                                  \
                                                             \
             break; }                                        
 
+    for (int i = 0; i < 2; ++i) {
 
-    while (content - FileBegin < FileInfoPtr -> st_size) {
+        for (int j = 0; j < LabelsNum; ++j)
+            if (LabelsArr[j].place == content - FileBegin)
+                printf ("%s\n", LabelsArr[j].name);
 
-        switch (*content) {
+        while (content - FileBegin < FileInfoPtr -> st_size) {
 
-            #include "commands.h"
+            switch (*content & CmdNumMask) {
 
-            default: printf ("unknown command (%d) (%x)", *content, content - FileBegin);
-                     return 1;
+                #include "commands.h"
+
+                default: printf ("unknown command (%d) (%x)", (*content & CmdNumMask), content - FileBegin);
+                         return 1;
+
+            }
 
         }
 
@@ -89,7 +110,7 @@ void PrintArgs (FILE* file, char** code, int numOfArgs) {
     assert (file);
     assert (code);
 
-    if (**code == CMD_push) {
+    if (**code  == CMD_push) {
         ++(*code);
         fprintf (file, "%lg ", (double) *((int*) *code) / Accuracy);
         *code += sizeof (int);
