aad6ab4ee8a33fde45a394a8a12b620b6edf0b2e
ItsMeMisha <m.shlyahin@rambler.ru>
2019-12-20 06:06:08 +0300

    fixes

--- MyLanguage/lang/middleend.h
+++ MyLanguage/lang/middleend.h
@@ -8,7 +8,9 @@
 
 void OptimiseTree (Tree* tree);
 void OptimiseNode (Tree_node* node);
-
+void OptimiseTwoArgs (Tree_node* node);
+void OptimiseOneArg  (Tree_node* node);
+ 
 void OptimiseTree (Tree* tree) {
 
     assert (tree);
@@ -39,49 +41,68 @@ void OptimiseNode (Tree_node* node) {
     if (node -> type != TOp)
         return;
 
-    if (node -> left == nullptr) {
+    if (node -> left == nullptr) 
+        OptimiseOneArg (node);
 
-        switch (node -> data.Odata) {
-
-            case OP_sin: node -> type = TNum; node -> data.Ndata = sin (node -> right -> data.Ndata); break;
-
-            case OP_cos: node -> type = TNum; node -> data.Ndata = cos (node -> right -> data.Ndata); break;
-
-            case OP_sqrt: node -> type = TNum; node -> data.Ndata = sqrt (node -> right -> data.Ndata); break;
-
-            default: return;
-
-        }
-
-        free (node -> right);
-        node -> right = nullptr;
-
-    } else if (node -> left -> type == TNum) {
-
-        switch (node -> data.Odata) {
-
-            case OP_add: node -> type = TNum; 
-                         node -> data.Ndata = node -> right -> data.Ndata + node -> left -> data.Ndata; break;
-            case OP_sub: node -> type = TNum; 
-                         node -> data.Ndata = node -> left -> data.Ndata - node -> right -> data.Ndata; break;
-            case OP_mul: node -> type = TNum; 
-                         node -> data.Ndata = node -> left -> data.Ndata * node -> right -> data.Ndata; break;
-            case OP_div: node -> type = TNum; 
-                         node -> data.Ndata = node -> left -> data.Ndata / node -> right -> data.Ndata; break;
-
-            default: return;
-
-        }
-
-        free (node -> right);
-        node -> right = nullptr;
-        free (node -> left);
-        node -> left = nullptr;
-
-    }
+    else if (node -> left -> type == TNum)
+        OptimiseTwoArgs (node);
 
     return;
     
+}
+
+void OptimiseTwoArgs (Tree_node* node) {
+
+    assert (node);
+    assert (node -> left);
+    assert (node -> right);
+
+    switch (node -> data.Odata) {
+
+        case OP_add: node -> type = TNum; 
+                     node -> data.Ndata = node -> right -> data.Ndata + node -> left -> data.Ndata; break;
+        case OP_sub: node -> type = TNum; 
+                     node -> data.Ndata = node -> left -> data.Ndata - node -> right -> data.Ndata; break;
+        case OP_mul: node -> type = TNum; 
+                     node -> data.Ndata = node -> left -> data.Ndata * node -> right -> data.Ndata; break;
+        case OP_div: node -> type = TNum; 
+                     node -> data.Ndata = node -> left -> data.Ndata / node -> right -> data.Ndata; break;
+
+        default: return;
+
+    }
+
+    free (node -> right);
+    node -> right = nullptr;
+    free (node -> left);
+    node -> left = nullptr;
+
+    return;
+
+}
+
+void OptimiseOneArg  (Tree_node* node) {
+
+    assert (node);
+    assert (node -> right);
+
+    switch (node -> data.Odata) {
+
+        case OP_sin: node -> type = TNum; node -> data.Ndata = sin (node -> right -> data.Ndata); break;
+
+        case OP_cos: node -> type = TNum; node -> data.Ndata = cos (node -> right -> data.Ndata); break;
+
+        case OP_sqrt: node -> type = TNum; node -> data.Ndata = sqrt (node -> right -> data.Ndata); break;
+
+        default: return;
+
+    }
+
+    free (node -> right);
+    node -> right = nullptr;
+
+    return;
+
 }
 
 #endif
