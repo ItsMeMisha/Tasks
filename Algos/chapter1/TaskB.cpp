2dd8a2c99173d9d70b6b7c5214592d3ee4ef1bdc
ItsMeMisha <m.shlyahin@rambler.ru>
2020-02-28 17:17:03 +0300

    TaskB

--- Algos/chapter1/TaskB.cpp
+++ Algos/chapter1/TaskB.cpp
@@ -1,22 +1,73 @@
-#include <stdio.h>
-#include <string.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 const int maxResLen = 10000;
 const int maxCount = 100;
+const int unknown = 0;
+
+void ReadPieces (std::ifstream& file, std::vector<std::string>& pieces, int& num);
+void SortPieces (std::vector<std::string>& pieces, const int& num);
+bool PiecesComp (const std::string pieceA, const std::string pieceB);
 
 int main () {
 
-    FILE* file = fopen ("number.in", "r");
+    std::vector<std::string> pieces;
+    int num = unknown;
+
+    std::ifstream InPut;
+    InPut.open("number.in");
+
+    ReadPieces (InPut, pieces, num);
+
+    InPut.close();
+
+    SortPieces (pieces, num);
     
+    std::ofstream OutPut;
+    OutPut.open ("number.out");
 
+    for (int i = 0; i < num; ++i)
+        OutPut << pieces[i];
 
-    fclose (file);
-
-    char* result = new char[maxResLen];
-    file = fopen ("number.out", "w");
-    fprintf ("%s", result);
-    delete[] result;
+    OutPut.close();
 
     return 0;
 
 }
+
+void ReadPieces (std::ifstream& file, std::vector<std::string>& pieces, int& num) {
+
+    std::string buf;
+    file >> buf;
+
+     while (!file.eof()) {
+
+        pieces.push_back(buf);
+        num++;
+        file >> buf;
+
+    } 
+
+    return;
+
+}
+ 
+void SortPieces (std::vector<std::string>& pieces, const int& num) {
+
+    std::sort (pieces.begin(), pieces.end(), PiecesComp);
+
+    return;
+
+}
+
+bool PiecesComp (const std::string pieceA, const std::string pieceB) {
+
+    std::string FullPiece1 = pieceA + pieceB;
+    std::string FullPiece2 = pieceB + pieceA;
+
+    return (FullPiece2.compare(FullPiece1) < 0);
+
+}
+
