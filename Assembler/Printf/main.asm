d43c7f0ff82e9758c84e36f5669195d44b861195
ItsMeMisha <m.shlyahin@rambler.ru>
2020-05-20 17:51:42 +0300

    check

--- Assembler/Printf/main.asm
+++ Assembler/Printf/main.asm
@@ -4,17 +4,51 @@
 
 section     .text
 
-_start:     mov     rax, 242d
-            push    rax
-            push    rax
-            push    rax
-            push    rax
+_start: 
 
-            mov     rdi, exmpl
-            push    rdi
-            mov     rdi, fmt
+;=======================================
+;TEST1
+;=======================================
+
+            push    127d
+            push    '!'
+            push    100d
+            push    3802d
+            mov     rsi, lovestr
+            push    rsi
+            mov     rdi, test1
             push    rdi   
             call    _myPrintf
+
+;=======================================
+;TEST2
+;=======================================
+
+            push    -8086d
+            push    -8086d
+            mov     rdi, test2
+            push    rdi   
+            call    _myPrintf
+;=======================================
+;TEST3
+;=======================================
+
+            push    8086d
+            push    8086d
+            mov     rdi, test3
+            push    rdi   
+            call    _myPrintf
+;=======================================
+;TEST4
+;=======================================
+
+            push    8086d
+            mov     rdi, test4
+            push    rdi   
+            call    _myPrintf
+;=======================================
+;End of tests
+;=======================================
 
             mov rax, 60
             xor rdi, rdi
@@ -22,8 +56,11 @@ _start:     mov     rax, 242d
 
 section     .data
 
-fmt         db  "hello, world!", 10, 13, "%s", 0
+test1       db "I %s %x %d%%%c%b", 10, 13, 0
+lovestr     db 'love', 0
 
-;, "%d", 10, 13, "%o", 10, 13, "%x", 10, 13, "%u", 10, 13, 0
+test2       db "Test2: %%d:%d, %%u:%u", 10, 13, 0
+test3       db "Test3: %%x:%x, %%o:%o", 10, 13, 0
+test4       db "Test4: %%b:%b", 10, 13, 0
 
-exmpl       db  "I will sleep tonight!", 10, 13, 0
+
